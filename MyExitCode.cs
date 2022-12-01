69cdbf2f05d3dbea5e31d8f9327891ae2460ccef
ShanKonduru <ShanKonduru@gmail.com>
2022-12-01 12:47:38 +0530

    hello world

---
+++ MyExitCode.cs
@@ -0,0 +1,6 @@
+public enum MyExitCode : int {
+  Success = 0,
+  InvalidLogin = 1,
+  InvalidFilename = 2,
+  UnknownError = 10
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rnd = new Random(10);
+int value = rnd.Next(1, 10);
+
+for (int i = 1; i <= 1000; i++)
+{
+    Console.WriteLine(i + " Hello, World!");
+}
+
+if(value<=5 )
+{
+    Console.WriteLine("Success");
+    return (int)MyExitCode.Success;
+}
+else
+{
+    Console.WriteLine("UnknownError");
+    return (int)MyExitCode.UnknownError;
+}
